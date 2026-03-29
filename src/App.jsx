acb10823d203bced0cfd4c5e2704ed5c6323e949
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-29 22:49:49 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <span className="loading loading-spinner loading-xl"></span>
+    </>
+  )
+}
+
+export default App
