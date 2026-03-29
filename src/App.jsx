bfb948f563d59742c55b8c1acff4c7c3e2e2501f
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-29 23:11:16 +0600

    project setup done 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl text-red-500 font-bold underline">
-        Hello world!
-      </h1>
-      <span className="loading loading-spinner loading-xl"></span>
-    </>
+    <div>
+      Hello World!
+    </div>
   )
 }
 
