4089046af7d611ebe3d6c82607cdac166ac2a59b
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-30 17:20:23 +0600

    transform  done

--- src/sections/Transform.jsx
+++ src/sections/Transform.jsx
@@ -7,9 +7,26 @@ const Transform = () => {
                 {/* section heading  */}
                 <div className='text-center'>
                     <h3 className='text-white text-[3rem] font-extrabold mb-4 leading-tight'>Ready to Transform Your Workflow?</h3>
-                    <p className='text-white opacity-80 text-base font-normal mb-4 leading-tight'>Join thousands of professionals who are already using Digitools to work smarter.<br/>
-Start your free trial today.</p>
+                    <p className='text-white opacity-80 text-base font-normal mb-4 leading-tight'>Join thousands of professionals who are already using Digitools to work smarter.<br />
+                        Start your free trial today.</p>
                 </div>
+
+                {/* section content */}
+                {/* buttons container */}
+                <div className='mt-10 flex items-center justify-center gap-4'>
+                    <button className={`w-40 h-12 bg-white rounded-full cursor-pointer`}>
+                        <span className={`bg-brand-gradient bg-clip-text text-transparent text-base font-semibold`}>Explore Products</span>
+                    </button>
+
+                    <button className={`w-40 h-12 bg-transparent border border-white  rounded-full cursor-pointer`}>
+                        <span className={` text-white text-base font-semibold` }>View Pricing</span>
+                    </button>
+                </div>
+                <ul className='mt-4 text-white opacity-80 text-base font-normal list-disc flex justify-center gap-8'>
+                    <li className='list-none'>14-day free trial</li>
+                    <li>No credit card required</li>
+                    <li>Cancel anytime</li>
+                </ul>
             </div>
         </div>
     )
