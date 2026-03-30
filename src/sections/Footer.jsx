5ed62408dbcf85273e9370f86da5bc5e184411ad
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-03-30 19:36:25 +0600

    footer  done

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,13 +1,80 @@
 import React from 'react'
 
+import instagramImg from "../assets/instagram.png";
+import facebookImg from "../assets/facebook.png";
+import twitterImg from "../assets/twitter.png";
+
+const footerLinksData = [
+    { label: 'Product', items: ['Features', 'Pricing', 'Templates', 'Integrations'] },
+    { label: 'Company', items: ['About', 'Blog', 'Careers', 'Press'] },
+    { label: 'Resources', items: ['Documentation', 'Help Center', 'Community', 'Contact'] },
+];
+
+const RenderQuickItem = ({ item }) => {
+    console.log(item);
+
+    return (
+        <div className='space-y-4 mt-4'>
+            <h2 className='text-white text-lg font-medium'>{item.label}</h2>
+            <ul className='space-y-4'>
+                {
+                    item.items.map(item => (
+                        <li className='text-white opacity-70 hover:opacity-100 transition-all duration-300 text-base font-normal'><a href="#">{item}</a></li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+};
+
 const Footer = () => {
     return (
         <footer className='w-full pt-30 pb-7.5 bg-[#101727]'>
-            <div className='max-w-300 mx-auto'>
-                {/* section heading  */}
-                <div className='text-center'>
-                    <h3 className='text-white text-[3rem] font-extrabold mb-4 leading-tight'>Simple, Transparent Pricing</h3>
-                    <p className='text-text-secondary text-base font-normal mb-4 leading-tight'>Choose the plan that fits your needs. Upgrade or downgrade anytime.</p>
+            <div className='max-w-300 mx-auto '>
+
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8 mb-20'>
+                    {/* brand info */}
+                    <div className='col-span-2'>
+                        <h2 className='text-white text-[3rem] font-extrabold leading-tight mb-4'>DigiTools</h2>
+                        <p className='text-white opacity-70 text-base font-normal leading-7'>Premium digital tools for creators,<br />professionals, and businesses. Work smarter <br />with our suite of powerful tools.</p>
+                    </div>
+
+                    {/* Links */}
+                    {
+                        footerLinksData.map((item, i) => <RenderQuickItem key={i} item={item} />)
+                    }
+
+                    {/* social */}
+                    <div className='space-y-4 mt-4'>
+                        <h2 className='text-white text-lg font-medium'>Social Links</h2>
+                        {/* social icons container  */}
+                        <div className='flex items-center gap-3'>
+                            {/* insta  */}
+                            <a href='#' className='w-fit p-2.5 bg-background rounded-full'>
+                                <img src={instagramImg} />
+                            </a>
+                            {/* fb  */}
+                            <a href='#' className='w-fit p-2.5 bg-background rounded-full'>
+                                <img src={facebookImg} />
+                            </a>
+                            {/* tt  */}
+                            <a href='#' className='w-fit p-2.5 bg-background rounded-full'>
+                                <img src={twitterImg} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='w-full h-px bg-background opacity-10' />
+
+                {/* bottom */}
+                <div className='mt-7.5 text-white opacity-50 text-base font-normal flex items-center justify-between'>
+                    <p>© 2026 DigiTools. All rights reserved.</p>
+                    <ul className='flex items-center gap-8'>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Cookies</a></li>
+                    </ul>
                 </div>
             </div>
         </footer>
